fb01a833eff1bd4ffd412bb1e61980ff54ca8342
Carol Yao <c.y-@hotmail.com>
2016-05-31 14:15:32 -0400

    practice problems for exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+# How would you calculate a good tip for a 55 dollar meal?
+
+puts "#{55*0.15}"
+
+# Try adding a string and integer with the + operator
+
+puts "Today is " + 23.to_s + " degrees."
+
+# Try outputting the result of 45628 multiplied by 7839 in a sentence using interpolation
+
+puts "#{45628*7839}"
+
+# What's the value of (10 < 20 && 30 < 20) || !(10 == 11)
+
+true
+# 10 == 11 is false and !false = true and || requires only 1 true to be true
