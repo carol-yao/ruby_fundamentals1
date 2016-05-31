961e99d5d5c42250268ecfe33e599d917e635ec5
Carol Yao <c.y-@hotmail.com>
2016-05-31 15:37:05 -0400

    Changed 2016 to current year in order for code to work in next years

--- exercise3.rb
+++ exercise3.rb
@@ -17,15 +17,15 @@ puts "What day is your birthday?"
 birth_day = gets.chomp
 
   if Time.now.month > birth_month.to_i
-    puts "You are born in #{2016 - age.to_i}."
+    puts "You are born in #{Time.now.year - age.to_i}."
   elsif Time.now.month == birth_month.to_i
     if Time.now.day > birth_day.to_i
-      puts "You are born in #{2016 - age.to_i}."
+      puts "You are born in #{Time.now.year - age.to_i}."
     elsif Time.now.day == birth_day.to_i
-      puts "You are born in #{2016 - age.to_i}"
+      puts "You are born in #{Time.now.year - age.to_i}"
     else
-      puts "You are born in #{(2016 - age.to_i) - 1}"
+      puts "You are born in #{(Time.now.year - age.to_i) - 1}"
     end
   else
-    puts "You are born in #{(2016 - age.to_i) - 1}."
+    puts "You are born in #{(Time.now.year - age.to_i) - 1}."
   end
