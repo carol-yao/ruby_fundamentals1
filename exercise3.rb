d081331e9aae975f8c4da69373f0f682e4674e1d
Carol Yao <c.y-@hotmail.com>
2016-05-31 14:58:42 -0400

    added ability to calculate birth year taking into account the month of the year

--- exercise3.rb
+++ exercise3.rb
@@ -8,4 +8,18 @@ puts "How old are you?"
 
 age = gets.chomp
 
-puts "You were born in #{2016 - age.to_i}"
+puts "What month are you born in (i.e. january = 1, december = 12)?"
+
+birth_month = gets.chomp
+
+puts "What day is your birthday?"
+
+birth_day = gets.chomp
+
+  if Time.now.month > birth_month.to_i
+    puts "You are born in #{2016 - age.to_i}."
+  elsif Time.now.month == birth_month.to_i
+    puts "this month is your birth month"
+  else
+    puts "You are born in #{(2016 - age.to_i) - 1}."
+  end
