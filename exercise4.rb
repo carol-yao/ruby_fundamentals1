6e7075bdd4eb444cddc9c15ad05de25429ff692d
Carol Yao <c.y-@hotmail.com>
2016-05-31 14:43:53 -0400

    Added if statement to determine multiples of 3, 5 and both

--- exercise4.rb
+++ exercise4.rb
@@ -1,5 +1,11 @@
 from_1_to_100 = (1..100)
 
 from_1_to_100.each do |number|
-  puts number
+  if number % 5 == 0 && number % 3 == 0
+    puts "BitMaker"
+  elsif number % 5 == 0
+    puts "Maker"
+  else
+    puts "Bit"
+  end
 end
